c8dcee8a2824ac00dc5781512cc07ba80b3e8868
histlog <safejp1@gmail.com>
2022-08-07 01:11:41 +0530

    Updated store

--- client/src/App.js
+++ client/src/App.js
@@ -10,25 +10,35 @@ import { Landing } from './components/layout/Landing';
 import { Register } from './components/auth/Register';
 import { Login } from './components/auth/Login';
 
+//redux
+import { Provider } from 'react-redux';
+import store from './store';
+
 import './App.css';
 
 const App = () => (
-  <Router>
-    <Fragment>
-      <Navbar></Navbar>
-      <Route exact path='/' render={(props) => <Landing {...props} />} />
-      <section className='container'>
-        <Switch>
-          <Route
-            exact
-            path='/register'
-            render={(props) => <Register {...props} />}
-          />
-          <Route exact path='/login' render={(props) => <Login {...props} />} />
-        </Switch>
-      </section>
-    </Fragment>
-  </Router>
+  <Provider store={store}>
+    <Router>
+      <Fragment>
+        <Navbar></Navbar>
+        <Route exact path='/' render={(props) => <Landing {...props} />} />
+        <section className='container'>
+          <Switch>
+            <Route
+              exact
+              path='/register'
+              render={(props) => <Register {...props} />}
+            />
+            <Route
+              exact
+              path='/login'
+              render={(props) => <Login {...props} />}
+            />
+          </Switch>
+        </section>
+      </Fragment>
+    </Router>
+  </Provider>
 );
 
 export default App;
