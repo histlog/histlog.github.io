66a020d01aefd8143f37a9dc4d808181cb98149f
histlog <safejp1@gmail.com>
2022-08-10 01:03:06 +0530

    Updated package & reducers

--- client/src/store.js
+++ client/src/store.js
@@ -1,5 +1,5 @@
-import { createStore, applyMiddleware } from 'redux/middleware';
-import { composeWithDevTools } from 'redux-devtools-extensions';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 
