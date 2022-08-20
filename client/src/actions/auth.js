dbbd6a0553b4d6b0837c7677add8d9f6de82c3e8
histlog <safejp1@gmail.com>
2022-08-21 00:40:49 +0530

    Updated register action

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { setAlert } from './alert';
 import { REGISTER_SUCCESS, REGISTER_FAIL } from './types';
 
 // Register User
@@ -14,5 +15,21 @@ export const register =
     const body = JSON.stringify({ name, email, password });
 
     try {
-    } catch (err) {}
+      const res = await axios.post('/api/users', body, config);
+
+      dispatch({
+        type: REGISTER_SUCCESS,
+        payload: res.data,
+      });
+    } catch (err) {
+      const errors = err.response.data.errors;
+
+      if (errors) {
+        errors.foreach((error) => dispatch(setAlert(error.msg, 'danger')));
+      }
+
+      dispatch({
+        type: REGISTER_FAIL,
+      });
+    }
   };
