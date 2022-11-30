b4a6a7020aab57f3f4ad6a304417bfaedc8c4c68
histlog <safejp1@gmail.com>
2022-12-01 01:02:20 +0530

    Updated form layout styling

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -4,21 +4,19 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { logout } from '../../actions/auth';
 
-
-const Navbar = ({auth: { isAuthenticated, loading }, logout}) => {
-
+const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
   const authLinks = (
     <ul>
       <li>
         <Link to='/dashboard'>
-        <i className="fas fa-user"></i>{' '}
-        <span className="hide-sm">Dashboard</span>
-          </Link>
+          <i className='fas fa-user'></i>{' '}
+          <span className='hide-sm'>Dashboard</span>
+        </Link>
       </li>
       <li>
         <a onClick={logout} href='#!'>
-          <i className="fas fa-sign-out-alt"></i>{' '}
-          <span className="hide-sm">Logout</span>
+          <i className='fas fa-sign-out-alt'></i>{' '}
+          <span className='hide-sm'>Logout</span>
         </a>
       </li>
     </ul>
@@ -45,18 +43,20 @@ const Navbar = ({auth: { isAuthenticated, loading }, logout}) => {
           <i className='fas fa-code'></i> DevConnector
         </Link>
       </h1>
-      {!loading && (<Fragment>{ isAuthenticated ? authLinks : guestLinks }</Fragment>)}
+      {!loading && (
+        <Fragment>{isAuthenticated ? authLinks : guestLinks}</Fragment>
+      )}
     </nav>
   );
 };
 
 Navbar.propTypes = {
   logout: PropTypes.func.isRequired,
-  auth: PropTypes.object.isRequired
-}
+  auth: PropTypes.object.isRequired,
+};
 
-const mapStateToProps = state => ({
-  auth: state.auth
+const mapStateToProps = (state) => ({
+  auth: state.auth,
 });
 
 export default connect(mapStateToProps, { logout })(Navbar);
--- client/src/components/profile-forms/EditProfile.js
+++ client/src/components/profile-forms/EditProfile.js
@@ -36,7 +36,7 @@ const EditProfile = ({
       website: loading || !profile.website ? '' : profile.website,
       location: loading || !profile.location ? '' : profile.location,
       skills: loading || !profile.skills ? '' : profile.skills,
-      githubusername: 
+      githubusername:
         loading || !profile.githubusername ? '' : profile.githubusername,
       bio: loading || !profile.bio ? '' : profile.bio,
       twitter: loading || !profile.twitter ? '' : profile.twitter,
@@ -72,26 +72,32 @@ const EditProfile = ({
 
   return (
     <Fragment>
-      <div className="pageWrap page-w-form px-3">
-        <div className="container">          
+      <div className='pageWrap page-w-form'>
+        <div className='container px-3'>
           <h1 className='large text-primary'>Create Your Profile</h1>
           <p className='lead'>
-            <i className='fas fa-user'></i> Let's get some information to make your
-            profile stand out
+            <i className='fas fa-user'></i> Let's get some information to make
+            your profile stand out
           </p>
           <small>* = required field</small>
         </div>
         <form className='form' onSubmit={(e) => onSubmit(e)}>
-          <div className="scroller page">
-            <div className="container">
+          <div className='scroller page'>
+            <div className='container px-3'>
               <div className='form-group'>
-                <select name='status' value={status} onChange={(e) => onChange(e)}>
+                <select
+                  name='status'
+                  value={status}
+                  onChange={(e) => onChange(e)}
+                >
                   <option value='0'>* Select Professional Status</option>
                   <option value='Developer'>Developer</option>
                   <option value='Junior Developer'>Junior Developer</option>
                   <option value='Senior Developer'>Senior Developer</option>
                   <option value='Manager'>Manager</option>
-                  <option value='Student or Learning'>Student or Learning</option>
+                  <option value='Student or Learning'>
+                    Student or Learning
+                  </option>
                   <option value='Instructor'>Instructor or Teacher</option>
                   <option value='Intern'>Intern</option>
                   <option value='Other'>Other</option>
@@ -145,7 +151,8 @@ const EditProfile = ({
                   onChange={(e) => onChange(e)}
                 />
                 <small className='form-text'>
-                  Please use comma separated values (eg. HTML,CSS,JavaScript,PHP)
+                  Please use comma separated values (eg.
+                  HTML,CSS,JavaScript,PHP)
                 </small>
               </div>
               <div className='form-group'>
@@ -168,87 +175,91 @@ const EditProfile = ({
                   value={bio}
                   onChange={(e) => onChange(e)}
                 ></textarea>
-                <small className='form-text'>Tell us a little about yourself</small>
+                <small className='form-text'>
+                  Tell us a little about yourself
+                </small>
               </div>
+
+              <div className='my-2'>
+                <button
+                  onClick={() => toggleSocialInputs(!displaySocialInputs)}
+                  type='button'
+                  className='btn btn-light'
+                >
+                  {displaySocialInputs
+                    ? `Hide Social Network Links`
+                    : `Add Social Network Links`}
+                </button>
+                <span>Optional</span>
+              </div>
+
+              {displaySocialInputs && (
+                <Fragment>
+                  <div className='form-group social-input'>
+                    <i className='fab fa-twitter fa-2x'></i>
+                    <input
+                      type='text'
+                      placeholder='Twitter URL'
+                      name='twitter'
+                      value={twitter}
+                      onChange={(e) => onChange(e)}
+                    />
+                  </div>
+
+                  <div className='form-group social-input'>
+                    <i className='fab fa-facebook fa-2x'></i>
+                    <input
+                      type='text'
+                      placeholder='Facebook URL'
+                      name='facebook'
+                      value={facebook}
+                      onChange={(e) => onChange(e)}
+                    />
+                  </div>
+
+                  <div className='form-group social-input'>
+                    <i className='fab fa-youtube fa-2x'></i>
+                    <input
+                      type='text'
+                      placeholder='YouTube URL'
+                      name='youtube'
+                      value={youtube}
+                      onChange={(e) => onChange(e)}
+                    />
+                  </div>
+
+                  <div className='form-group social-input'>
+                    <i className='fab fa-linkedin fa-2x'></i>
+                    <input
+                      type='text'
+                      placeholder='Linkedin URL'
+                      name='linkedin'
+                      value={linkedin}
+                      onChange={(e) => onChange(e)}
+                    />
+                  </div>
+
+                  <div className='form-group social-input'>
+                    <i className='fab fa-instagram fa-2x'></i>
+                    <input
+                      type='text'
+                      placeholder='Instagram URL'
+                      name='instagram'
+                      value={instagram}
+                      onChange={(e) => onChange(e)}
+                    />
+                  </div>
+                </Fragment>
+              )}
             </div>
-
-            <div className='my-2'>
-              <button
-                onClick={() => toggleSocialInputs(!displaySocialInputs)}
-                type='button'
-                className='btn btn-light'
-              >
-                {displaySocialInputs
-                  ? `Hide Social Network Links`
-                  : `Add Social Network Links`}
-              </button>
-              <span>Optional</span>
-            </div>
-
-            {displaySocialInputs && (
-              <Fragment>
-                <div className='form-group social-input'>
-                  <i className='fab fa-twitter fa-2x'></i>
-                  <input
-                    type='text'
-                    placeholder='Twitter URL'
-                    name='twitter'
-                    value={twitter}
-                    onChange={(e) => onChange(e)}
-                  />
-                </div>
-
-                <div className='form-group social-input'>
-                  <i className='fab fa-facebook fa-2x'></i>
-                  <input
-                    type='text'
-                    placeholder='Facebook URL'
-                    name='facebook'
-                    value={facebook}
-                    onChange={(e) => onChange(e)}
-                  />
-                </div>
-
-                <div className='form-group social-input'>
-                  <i className='fab fa-youtube fa-2x'></i>
-                  <input
-                    type='text'
-                    placeholder='YouTube URL'
-                    name='youtube'
-                    value={youtube}
-                    onChange={(e) => onChange(e)}
-                  />
-                </div>
-
-                <div className='form-group social-input'>
-                  <i className='fab fa-linkedin fa-2x'></i>
-                  <input
-                    type='text'
-                    placeholder='Linkedin URL'
-                    name='linkedin'
-                    value={linkedin}
-                    onChange={(e) => onChange(e)}
-                  />
-                </div>
-
-                <div className='form-group social-input'>
-                  <i className='fab fa-instagram fa-2x'></i>
-                  <input
-                    type='text'
-                    placeholder='Instagram URL'
-                    name='instagram'
-                    value={instagram}
-                    onChange={(e) => onChange(e)}
-                  />
-                </div>
-              </Fragment>
-            )}
           </div>
-          <div className="container">
+          <div className='container px-3'>
             <input type='submit' className='btn btn-primary my-1' />
-            <a className='btn btn-light my-1' href='dashboard.html'>
-              Go Back
-            </a>
+            <Link to='/dashboard'>
+              <a className='btn btn-light my-1' href='#!'>
+                Go Back
+              </a>
+            </Link>
           </div>
         </form>
       </div>
